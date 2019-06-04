2ab958c9d9048ed85e5058e20f71a8124203d2aa
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-04 17:38:25 -0400

    Updates

---
+++ precache-manifest.ffdf51f3df8c6116db56c24bfdaa5c3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af751eb1d24773de64c0dbe2e56b529",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "da0ac5f28a0760770de5",
+    "url": "/memory-game/static/css/main.34a921b3.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "da0ac5f28a0760770de5",
+    "url": "/memory-game/static/js/main.bc7f7456.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
